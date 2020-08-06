136cf5cef1af29bd17d349c16a524b15fe08022f
Scott Salvaggio <scottsalvaggio@gmail.com>
2020-08-06 16:52:46 -0700

    Configure DOMPurify to strip out all HTML tags of request query and body

--- libs/dom.js
+++ libs/dom.js
@@ -42,7 +42,8 @@ exports.sanitize_req_body = function(req, res, next) {
         if (req.body.hasOwnProperty(prop)) {
 
             if (prop !== 'is_active' && typeof req.body[prop] === 'string') {
-                req.body[prop] = DOMPURIFY.sanitize(req.body[prop]);
+                req.body[prop] = DOMPURIFY.sanitize(req.body[prop],
+                                                    {ALLOWED_TAGS: []});
             }
         }
     });
@@ -67,7 +68,8 @@ exports.sanitize_req_query = function(req, res, next) {
     keys.map(function (prop) {
 
         if (req.query.hasOwnProperty(prop) && typeof req.query[prop] === 'string') {
-            req.query[prop] = DOMPURIFY.sanitize(req.query[prop]);
+            req.query[prop] = DOMPURIFY.sanitize(req.query[prop],
+                                                 {ALLOWED_TAGS: []});
         }
     });
 
