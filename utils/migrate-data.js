ebeb9a40d418daa756679539e12b2d58e59c48d8
Scott Salvaggio <scottsalvaggio@gmail.com>
2020-09-16 17:36:03 -0700

    Use async.waterfall approach

--- utils/migrate-data.js
+++ utils/migrate-data.js
@@ -21,8 +21,9 @@
 require('dotenv').config({ path: '../.env' });
 
 const ASYNC = require('async'),
+      CONFIG = require('../config/config'),
       DB = require('../config/db')(),
-      CONFIG = require('../config/config');
+      LOGGER = require('../libs/log4');
 
 if (typeof CONFIG.dbOrigDonorTable === 'undefined') {
     console.log('ERROR: CONFIG.dbOrigDonorTable is undefined. Make sure you ' +
@@ -31,6 +32,51 @@ if (typeof CONFIG.dbOrigDonorTable === 'undefined') {
 
 let id = 10;
 
+// 1.)
+function query_donor_and_donation_amount(callback) {
+    let obj = {};
+
+    DB(CONFIG.dbOrigDonorTable)
+        .join(CONFIG.dbOrigDonationAmountTable,
+              CONFIG.dbOrigDonorTable + '.donorID', '=',
+              CONFIG.dbOrigDonationAmountTable + '.donorID')
+        .select('*')
+        .where(CONFIG.dbOrigDonorTable + '.donorID', id)
+        .then(function (data) {
+            console.log('----Donor----');
+            console.log(data);
+            obj.donor = data;
+            callback(null, obj);
+            return false;
+        })
+        .catch(function (error) {
+            LOGGER.module().error('ERROR [/utils/migrate-data module ' +
+                                  '(query_donor_and_donation_amount)] Error ' +
+                                  'when migrating data for id ' + id + ': ' +
+                                  error);
+        });
+}
+
+// 2.)
+function query_subject_area(obj, callback) {
+    DB(CONFIG.dbOrigDonationSubjectAreaTable)
+        .select('donorID', 'subject')
+        .where('donorID', id)
+        .then(function (data) {
+            console.log('\n----Subject Areas----');
+            console.log(data);
+            obj.subject_areas = data;
+            callback(null, obj);
+            return false;
+        })
+        .catch(function (error) {
+            LOGGER.module().error('ERROR [/utils/migrate-data module ' +
+                                  '(query_subject_area)] Error when migrating ' +
+                                  'data for id ' + id + ': ' + error);
+        });
+}
+
+/*
 DB(CONFIG.dbOrigDonorTable)
     .join(CONFIG.dbOrigDonationAmountTable,
           CONFIG.dbOrigDonorTable + '.donorID', '=',
@@ -88,3 +134,22 @@ DB(CONFIG.dbOrigBookTable)
     .catch(function (error) {
         console.log('Error: ' + error);
     });
+*/
+
+ASYNC.waterfall([
+   query_donor_and_donation_amount,
+   query_subject_area
+], function (error, results) {
+    console.log("\nInside waterfall function");
+    console.log("result = ");
+    console.log(results);
+
+    if (error) {
+        LOGGER.module().error('ERROR: [/utils/migrate-data module ' +
+                              '(async.waterfall)] Error when migrating data ' +
+                              'for id ' + id + ': ' + error);
+    }
+
+    console.log('\nEnd of migration attempt for record ' + id + '\n' +
+                '=====================\n');
+});
