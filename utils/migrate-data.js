8b3f509ed130adea6dcac2c8a780cbb1b5df97d2
Scott Salvaggio <scottsalvaggio@gmail.com>
2020-09-18 13:33:17 -0700

    Build who_to_notify array of objects

--- utils/migrate-data.js
+++ utils/migrate-data.js
@@ -24,7 +24,7 @@ const ASYNC = require('async'),
       CONFIG = require('../config/config'),
       DB = require('../config/db')();
 
-let id = 10;
+let id = 13;
 let error_msg_color = '\x1b[31m%s\x1b[0m', // red
     warning_msg_color = '\x1b[35m%s\x1b[0m', // magenta
     error_msg_text = 'Error when migrating data for id ' + id;
@@ -53,7 +53,8 @@ function query_donor_and_donation_amount(callback) {
                 'donorZip as donor_zip',
                 'amountOfDonation as donor_amount_of_donation',
                 'dateOfDonation as donor_date_of_donation',
-                'donorNotes as donor_notes')
+                'donorNotes as donor_notes',
+                CONFIG.dbOrigDonorTable + '.timestamp as created')
         .where(CONFIG.dbOrigDonorTable + '.donorID', id)
         .then(function (data) {
             console.log('----Donor----');
@@ -65,12 +66,14 @@ function query_donor_and_donation_amount(callback) {
                 return false;
             } else {
                 obj.id = data[0].id;
+                obj.created = data[0].created;
                 obj.donor = {};
                 console.log('Data for donor ' + data[0].id + ':');
                 for (let property in data[0]) {
                     console.log(property + ' = ' + data[0][property]);
-                    if (property !== 'id') {
-                        obj.donor[property] = data[0][property];
+                    if (property !== 'id' && property !== 'created') {
+                        obj.donor[property] =
+                            trim_value_if_string(data[0][property]);
                     }
                 }
             }
@@ -97,7 +100,7 @@ function query_subject_area(obj, callback) {
             }
             let subject_areas = [];
             for (let subject of data) {
-                subject_areas.push(subject.subject);
+                subject_areas.push(trim_value_if_string(subject.subject));
             }
             console.log('subject_areas = ');
             console.log(subject_areas);
@@ -111,32 +114,59 @@ function query_subject_area(obj, callback) {
         });
 }
 
+// 3.)
+function query_who_to_notify(obj, callback) {
+    DB(CONFIG.dbOrigNotifyTable)
+        .select('notifyTitle as notify_title',
+                'notifyFirstName as notify_first_name',
+                'notifyLastName as notify_last_name',
+                'notifyAddress as notify_address',
+                'notifyCity as notify_city',
+                'notifyState as notify_state',
+                'notifyZip as notify_zip',
+                'notifyRelationToDonor as notify_relation_to_donor')
+        .where('donorID', id)
+        .then(function (data) {
+            console.log('\n----Person to Notify----');
+            console.log(data);
+            if (data.length === 0) {
+                console.warn(warning_msg_color, 'WARNING [query_who_to_notify '
+                             + 'function]: Knex query returned 0 results.');
+            }
+            let who_to_notify = [];
+            for (let person_to_notify of data) {
+                let containsNonEmptyElementValue = false,
+                    person_obj = {};
+                for (let property in person_to_notify) {
+                    console.log(property + ' = ' + person_to_notify[property]);
+                    person_obj[property] =
+                        trim_value_if_string(person_to_notify[property]);
+                    if (person_obj[property] !== '') {
+                        containsNonEmptyElementValue = true;
+                    }
+                }
+                if (containsNonEmptyElementValue) {
+                    who_to_notify.push(person_obj);
+                } else {
+                    console.warn(warning_msg_color, 'WARNING [query_who_to_' +
+                                 'notify function]: id ' + id + ' contains ' +
+                                 'empty record in ' + CONFIG.dbOrigNotifyTable +
+                                 ' table.');
+                }
+            }
+            console.log('who_to_notify = ');
+            console.log(who_to_notify);
+            obj.who_to_notify = who_to_notify;
+            callback(null, obj);
+            return false;
+        })
+        .catch(function (error) {
+            console.error(error_msg_color, 'ERROR [query_who_to_notify ' +
+                          'function]: ' + error_msg_text + ': ' + error);
+        });
+}
+
 /*
-DB(CONFIG.dbOrigDonorTable)
-    .join(CONFIG.dbOrigDonationAmountTable,
-          CONFIG.dbOrigDonorTable + '.donorID', '=',
-          CONFIG.dbOrigDonationAmountTable + '.donorID')
-    .select('*')
-    .where(CONFIG.dbOrigDonorTable + '.donorID', id)
-    .then(function (data) {
-        console.log('----Donor----');
-        console.log(data);
-    })
-    .catch(function (error) {
-        console.log('Error: ' + error);
-    });
-
-DB(CONFIG.dbOrigDonationSubjectAreaTable)
-    .select('donorID', 'subject')
-    .where('donorID', id)
-    .then(function (data) {
-        console.log('\n----Subject Areas----');
-        console.log(data);
-    })
-    .catch(function (error) {
-        console.log('Error: ' + error);
-    });
-
 DB(CONFIG.dbOrigNotifyTable)
     .select('*')
     .where('donorID', id)
@@ -173,11 +203,13 @@ DB(CONFIG.dbOrigBookTable)
 
 ASYNC.waterfall([
    query_donor_and_donation_amount,
-   query_subject_area
+   query_subject_area,
+   query_who_to_notify
 ], function (error, results) {
-    console.log("\nInside waterfall function");
-    console.log("result = ");
+    console.log('\nInside waterfall function');
+    console.log('results (as object) = ');
     console.log(results);
+    console.log('results (as stringified JSON) = ' + JSON.stringify(results));
 
     if (error) {
         console.error(error_msg_color, 'ERROR [async.waterfall]: ' +
@@ -187,3 +219,13 @@ ASYNC.waterfall([
     console.log('\nEnd of migration attempt for record ' + id + '\n' +
                 '=====================\n');
 });
+
+/**
+ * If string, returns trimmed value; if not a string, returns value as is
+ * @param    {(number|string)}    value    the value to check
+ * @returns  {(number|string)}             the trimmed string (if value is
+ *                                         string); otherwise, the value as is
+ */
+const trim_value_if_string = function (value) {
+    return typeof value === 'string' ? value.trim() : value;
+};
